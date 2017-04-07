4408a13e131c1b08cf828ce07bcd90c15aed6bcd
Dillon Niederhut <dillon.niederhut@gmail.com>
2017-04-07 17:37:12 -0500

    adds numeric objects to dunder init

--- tram/__init__.py
+++ tram/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 
-from tram.objects import (List, Dict)
+from tram.objects import (Dict, Float, Int, List)
 from tram.functions import (transfer_value, transfer_item)
